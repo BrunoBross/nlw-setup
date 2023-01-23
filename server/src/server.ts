7167d80e0e2302d80af6d301a95ec66cc0d0dd45
BrunoBross <brunobarretobruno@hotmail.com>
2023-01-23 00:11:36 -0300

    teste variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,13 +4,15 @@ import { appRoutes } from "./routes";
 
 const app = Fastify();
 
+const PORT = process.env.PORT || 8080;
+
 app.register(cors);
 
 app.register(appRoutes);
 
 app
   .listen({
-    port: 3333,
+    port: PORT,
     host: "0.0.0.0",
   })
   .then(() => {
